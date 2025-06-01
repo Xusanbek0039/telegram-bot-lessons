19b61cb1e09b46e0e3fd837b39643313bcdf0cfd
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-06-02 02:22:52 +0500

    Update main.py

---
+++ 01-dars aiogram bilan tanishuv hamda u bilan ishlash/main.py
@@ -0,0 +1,26 @@
+import asyncio
+from os import getenv
+from dotenv import load_dotenv  # 👈 BU YANGI QATOR
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+load_dotenv()  # 👈 BU YERDA .env faylni yuklaymiz
+
+TOKEN = getenv("BOT_TOKEN")
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot created with aiogram.")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
